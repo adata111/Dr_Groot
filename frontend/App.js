4271e0e17fae5ae1882de1a429c66c848211ee0a
Ahana Datta <ahana.datta.ravenclaw@gmail.com>
2021-08-09 22:55:47 +0100

    improved camera

--- frontend/App.js
+++ frontend/App.js
@@ -1,145 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
+// import React, { useState, useEffect } from 'react';
+// import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+// import { Camera } from 'expo-camera';
+// import Home from 'home.js';
 
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
+import 'react-native-gesture-handler';
+import * as React from "react";
+import { StyleSheet,Text, View, TouchableOpacity, ImageBackground} from "react-native";
+import { NavigationContainer, CommonActions } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+import CameraPage from './pages/camera.js';
+import Gallery from './pages/gallery.js';
+import Home from './pages/home.js';
+
+const Stack = createStackNavigator();
+
+export default function App(){
+  
   return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-    </View>
-  );
+
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Camera" component={CameraPage}/>
+        <Stack.Screen name="Home" component={Home}/>
+        {/* <Stack.Screen name="Gallery" component={Gallery}/> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    margin: 20,
-  },
-  button: {
-    flex: 0.1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-    color: 'white',
-  },
+   flex: 1,
+   alignSelf: 'stretch',
+   alignItems: 'center',
+   backgroundColor: 'rgba(255,255,255,0.3)',
+   justifyContent: 'center',
+   paddingLeft: 60,
+   paddingRight: 60,
+ },
 });
-
-// import { StatusBar } from 'expo-status-bar';
-// import React, { useState } from 'react';
-// import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
-// import { RNCamera } from 'react-native-camera'
-// import CameraPage from './pages/camera';
-
-// export default function App() {
-//   return (
-//     <View style={styles.screen}>
-//       <SafeAreaView style={styles.saveArea}>
-//         <View style={styles.topBar}>
-//           <Text style={styles.topBarTitleText}>React Native Scanner</Text>
-//         </View>
-//       </SafeAreaView>
-
-//       <View style={styles.caption}>
-//         <Text style={styles.captionTitleText}>Welcome To React-Native-Camera Tutorial</Text>
-//       </View>
-
-//       <RNCamera 
-//         //   ref={ref => {
-//         //     this.camera = ref;
-//         // }} 
-//         style={styles.rnCamera} 
-//         type={RNCamera.Constants.Type.back}
-//         flashMode={RNCamera.Constants.FlashMode.on}
-//         captureAudio={false}
-//         androidCameraPermissionOptions={{
-//           title: 'Permission to use camera',
-//           message: 'We need your permission to use your camera',
-//           buttonPositive: 'Ok',
-//           buttonNegative: 'Cancel',
-//         }}
-//         // androidRecordAudioPermissionOptions={{
-//         //   title: 'Permission to use audio recording',
-//         //   message: 'We need your permission to use your audio',
-//         //   buttonPositive: 'Ok',
-//         //   buttonNegative: 'Cancel',
-//         // }}
-//       />
-
-//       <View style={styles.cameraControl}>
-//         <TouchableOpacity style={styles.btn}>
-//           <Text style={styles.btnText}>New QR Scan</Text>
-//         </TouchableOpacity>
-//       </View>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   topBar: {
-//     height: 50,
-//     backgroundColor: '#62d1bc', // green
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   topBarTitleText: {
-//     color: '#ffffff', // white
-//     fontSize: 20,
-//   },
-//   caption: {
-//     height: 120,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   captionTitleText: {
-//     color: '#121B0D', // black
-//     fontSize: 16,
-//     fontWeight: '600'
-//   },
-//   rnCamera: {
-//     flex: 1,
-//     width: '94%',
-//     alignSelf: 'center',
-//   }
-// });
 
 
---
+++ frontend/pages/home.js
@@ -0,0 +1,94 @@
+import 'react-native-gesture-handler';
+import * as React from "react";
+import { StyleSheet,Text, View, TouchableOpacity, ImageBackground} from "react-native";
+import {Camera} from 'expo-camera';
+export default function Home({ navigation }){
+
+    const [startCamera, setStartCamera] = React.useState(false)
+    const __startCamera = async () => {
+        const {status} = await Camera.requestPermissionsAsync()
+        console.log(status)
+        if (status === 'granted') {
+        //   setStartCamera(true);
+          navigation.navigate('Camera');
+        } else {
+          Alert.alert('Access denied')
+        }
+      }
+  return (
+    <ImageBackground
+        source={require('./home1.jpg')}
+        style={styles.back}>
+        <View style={styles.container}>
+            <Text style={styles.title}>Dr Groot</Text>
+            <Text style={styles.subtitle}>Your Personal</Text>
+            <Text style={styles.subtitle}>Plant Doctor</Text>
+            <TouchableOpacity style={ styles.button }>
+                <Text style={ styles.buttonText } onPress={__startCamera}>Check My Plant</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={ styles.button }>
+                <Text style={ styles.buttonText }>Gallery</Text>
+            </TouchableOpacity>
+        </View>
+    </ImageBackground>
+   );
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   alignSelf: 'stretch',
+   alignItems: 'center',
+   backgroundColor: 'rgba(255,255,255,0.3)',
+   justifyContent: 'center',
+   paddingLeft: 60,
+   paddingRight: 60,
+ },
+
+ button: {
+    alignItems: "center",
+    paddingVertical: 10,
+    margin: 5,
+    paddingHorizontal: 15,
+    borderRadius: 25,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#2ba189',
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+
+  buttonText: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+
+  back: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+
+   title: {
+      fontWeight: 'bold',
+      fontSize: 40,
+      paddingBottom: 10,
+      marginBottom: 20,
+      borderBottomColor: 'black',
+      borderBottomWidth: 1,
+  },
+
+  subtitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingBottom: 10,
+    marginBottom: 5,
+},
+ });
+
+
+
